aaa8a4c0e8b8e0ba9375bd427f1eded1908617d9
Ayanendu.De <ayanendu.de@hiscox.com>
2019-09-25 21:05:23 -0400

    no message

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -9,6 +9,9 @@ public class App
     private final String message = "Hello World!";
 
     public App() {}
+    App A1 = new App();
+    App A2 = new App();
+
 
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
